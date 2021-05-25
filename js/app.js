be1e96ede4912908ccdd0b52c655cc7bf8d16810
Ahmad-loop98 <ahmadals3adi@gmail.com>
2021-05-25 15:44:30 +0300

    modified other website using java script and html

--- js/app.js
+++ js/app.js
@@ -21,10 +21,42 @@ if(age=='yes'||age=='y'){
 alert('thought you older');
 }
 console.log(' age'+ age);
-let novels=prompt("do you like novels ?");
+let novels=prompt("do you i like novels ?");
 novels=novels.toLocaleUpperCase();
 console.log('novels liked'+ novels);
-let myName=prompt("what's my name?");
-console.log('my name is'+ myName);
-myName =myName.toLowerCase();
+if(novels=='yes'||novels=='y'){
+    alert('awesome');
 
+}
+else if (novels=='no'||novels=='n'){
+    alert('not expected'); 
+}
+
+let myLisence=prompt("so you think i can drive ?");
+console.log('my lisence'+ myLisence);
+myLisence =myLisence.toLowerCase();
+if(myLisence=='yes'||myLisence=='y'){
+    alert('awesome');
+
+}
+else if (myLisence=='no'||myLisence=='n'){
+    alert('you guessed wrong'); 
+}
+let myWork=prompt("do i worked before ").toLocaleLowerCase
+console.log('my work was'+myWork);
+switch(myWork){
+    case'yes':
+    case'y':
+alert('good job');
+break;
+case'no':
+case'n':
+alert('no it isnt');
+break;
+default:
+    alert('you tried in a good way');
+
+}
+let userName=prompt("whats your name?")
+console.log('your name'+userName);
+alert(`my dav drink ${avDrink} and my generation${age} and likeable novels ${novels} and lisence${myLisence}and work${myWork} `);
